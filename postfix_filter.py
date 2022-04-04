1ce5e3507d48262182aca0aae13abc96e1db9f0a
César Richard <cesar.richard@weezevent.com>
2022-04-04 17:54:25 +0200

    Init CI

--- postfix_filter.py
+++ postfix_filter.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-import sys
-import fileinput
 import email
-f = open("/tmp/postfixtest1", "a")
-#f.write("The arguments are: {}\n\n".format(str(sys.argv)))
-#for line in sys.stdin.readlines():
-#    f.write(line)
-b = email.message_from_file(sys.stdin)
-#f.write(b['subject'])
-f.write('\n')
-body = ""
+import sys
 
+f = open("/tmp/postfixtest1", "a")
+b = email.message_from_file(sys.stdin)
+if b["subject"].contains("[GAR_"):
+    f.write("\nET NOUS AVONS UN VAINCEUR\n")
+
+body = ""
 if b.is_multipart():
     for payload in b.get_payload():
         # if payload.is_multipart(): ...
-        body = payload.get_payload()
+        body += payload.get_payload()
 else:
     body = b.get_payload()
 f.write(str(body))
-f.write('\n')
+f.write("\n")
 f.close()
