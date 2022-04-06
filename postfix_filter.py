01c6244b23c23d97764a24069deb57f6a2077829
César Richard <cesar.richard@weezevent.com>
2022-04-06 21:49:47 +0200

    Added token creation and sending

--- postfix_filter.py
+++ postfix_filter.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 import email
+import os
 import sys
+import jwt
+import requests
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
-f = open("/tmp/postfixtest1", "a")
+load_dotenv()
+
 b = email.message_from_file(sys.stdin)
-if b["subject"].contains("[GAR_"):
-    f.write("\nET NOUS AVONS UN VAINCEUR\n")
-
-body = ""
-if b.is_multipart():
-    for payload in b.get_payload():
-        # if payload.is_multipart(): ...
-        body += payload.get_payload()
-else:
-    body = b.get_payload()
-f.write(str(body))
-f.write("\n")
-f.close()
+if "[GAR_" in b["subject"]:
+    body = ""
+    if b.is_multipart():
+        for payload in b.get_payload():
+            body += payload.get_payload()
+    else:
+        body = b.get_payload()
+    encoded = jwt.encode(
+        {
+            "subject": b["subject"],
+            "from": b["from"],
+            "to": b["to"],
+            "body": BeautifulSoup(body, features="html.parser").get_text("\n"),
+        },
+        os.environ.get("PRIVATE_KEY"),
+        algorithm="RS256",
+    )
+    payload = {"agent": "MK_MAIL", "token": encoded}
+    requests.post(os.environ.get("GESASSO_LISTENER_URL"), data=payload)
