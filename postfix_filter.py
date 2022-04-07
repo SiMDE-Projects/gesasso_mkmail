0aaa3c0b42403aeb409a7417b8f25b5e005f49d3
César Richard <cesar.richard@weezevent.com>
2022-04-07 10:09:12 +0200

    Missing mail destination condition

--- postfix_filter.py
+++ postfix_filter.py
@@ -10,21 +10,22 @@ from bs4 import BeautifulSoup
 load_dotenv()
 
 b = email.message_from_file(sys.stdin)
-body = ""
-if b.is_multipart():
-    for payload in b.get_payload():
-        body += payload.get_payload()
-else:
-    body = b.get_payload()
-encoded = jwt.encode(
-    {
-        "subject": b["subject"],
-        "from": b["from"],
-        "to": b["to"],
-        "body": BeautifulSoup(body, features="html.parser").get_text("\n"),
-    },
-    os.environ.get("PRIVATE_KEY"),
-    algorithm="RS256",
-)
-payload = {"agent": "MK_MAIL", "token": encoded}
-requests.post(os.environ.get("GESASSO_LISTENER_URL"), data=payload)
+if "assos.utc.fr" in b["to"] and ("simde" in b["to"] or "payutc" in b["to"]):
+    body = ""
+    if b.is_multipart():
+        for payload in b.get_payload():
+            body += payload.get_payload()
+    else:
+        body = b.get_payload()
+    encoded = jwt.encode(
+        {
+            "subject": b["subject"],
+            "from": b["from"],
+            "to": b["to"],
+            "body": BeautifulSoup(body, features="html.parser").get_text("\n"),
+        },
+        os.environ.get("PRIVATE_KEY"),
+        algorithm="RS256",
+    )
+    payload = {"agent": "MK_MAIL", "token": encoded}
+    requests.post(os.environ.get("GESASSO_LISTENER_URL"), data=payload)
