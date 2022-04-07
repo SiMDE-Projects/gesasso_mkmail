27bb6b219bf1e2411c8c91f671953c48c860611b
César Richard <cesar.richard@weezevent.com>
2022-04-07 03:22:56 +0200

    removed subject filter

--- postfix_filter.py
+++ postfix_filter.py
@@ -10,22 +10,21 @@ from bs4 import BeautifulSoup
 load_dotenv()
 
 b = email.message_from_file(sys.stdin)
-if "[GAR_" in b["subject"]:
-    body = ""
-    if b.is_multipart():
-        for payload in b.get_payload():
-            body += payload.get_payload()
-    else:
-        body = b.get_payload()
-    encoded = jwt.encode(
-        {
-            "subject": b["subject"],
-            "from": b["from"],
-            "to": b["to"],
-            "body": BeautifulSoup(body, features="html.parser").get_text("\n"),
-        },
-        os.environ.get("PRIVATE_KEY"),
-        algorithm="RS256",
-    )
-    payload = {"agent": "MK_MAIL", "token": encoded}
-    requests.post(os.environ.get("GESASSO_LISTENER_URL"), data=payload)
+body = ""
+if b.is_multipart():
+    for payload in b.get_payload():
+        body += payload.get_payload()
+else:
+    body = b.get_payload()
+encoded = jwt.encode(
+    {
+        "subject": b["subject"],
+        "from": b["from"],
+        "to": b["to"],
+        "body": BeautifulSoup(body, features="html.parser").get_text("\n"),
+    },
+    os.environ.get("PRIVATE_KEY"),
+    algorithm="RS256",
+)
+payload = {"agent": "MK_MAIL", "token": encoded}
+requests.post(os.environ.get("GESASSO_LISTENER_URL"), data=payload)
